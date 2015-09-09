c45fc418fe333ce7a34633e68e9c4dd4ee244716
HyunMi027 <ketsi027@gmail.com>
2015-09-09 18:53:45 +0200

    Q_Q

--- Kekplank/Kekplank/Properties/AssemblyInfo.cs
+++ Kekplank/Kekplank/Properties/AssemblyInfo.cs
@@ -12,5 +12,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("3ff74961-cd94-40e9-be5a-1a331e7bfc61")]
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyVersion("2.0.2.*")]
+[assembly: AssemblyFileVersion("2.0.2.1")]
